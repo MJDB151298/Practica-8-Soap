f35945c1365fc31f8be43e222c909b44157ccb63
marco <20160370@ce.pucmm.edu.do>
2019-12-09 10:13:26 -0400

    Initial commit

---
+++ practica8_soap.py
@@ -0,0 +1,35 @@
+from zeep import Client
+
+client = Client('http://localhost:7777/ws/AcademicoWebService?wsdl')
+
+
+def getEstudiantes(servicio):
+    return {
+        servicio: client.service.getAllEstudiante()
+    }[servicio]
+
+
+def getAsignatura(id):
+    return {"asignatura": client.service.getAsignatura(id)}["asignatura"]
+
+
+def getProfesor(id):
+    return {"profesor": client.service.getProfesor(id)}["profesor"]
+
+
+while True:
+    opcion = int(input("1- Listar estudiantes. \n"
+                   "2- Buscar asignatura. \n"
+                   "3- Buscar Profesor. \n"
+                   "0- Salir. \n"
+                   "Opcion: "))
+    if opcion == 0:
+        break
+    elif opcion == 1:
+        print(getEstudiantes("estudiantes"))
+    elif opcion == 2:
+        asignatura = input("Digite el id de la asignatura a consultar: ")
+        print(getAsignatura(asignatura))
+    elif opcion == 3:
+        profesor = input("Digite el id del profesor a consultar: ")
+        print(getProfesor(profesor))
